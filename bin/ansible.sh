43c3550b30263c7fdf43d714ff82a55a08c9113a
Ivan Chavero <ichavero@redhat.com>
2016-03-15 17:40:20 -0700

    Add a function to clone a virtual machine

--- bin/ansible.sh
+++ bin/ansible.sh
@@ -12,6 +12,17 @@ SLEEP=$7
 RELEASE=$8
 TAG_SWITCH=""
 
+
+function create_vm {
+    ./clone $1 $2
+    ./start $NAME
+    echo "Waiting $SLEEP seconds for the VM to come up"
+    sleep $SLEEP
+    IP=`./virt-addr.pl $NAME`
+    echo $IP >> ./packstack-test-hosts
+}
+
+
 if [[ ${TEMPLATE} == "" ]]; then
     TEMPLATE="CentOS-7.0-Template"
 fi
@@ -36,13 +47,11 @@ if [[ "${SLEEP}" == "" ]]; then
     SLEEP=60
 fi
 
-./clone $TEMPLATE $NAME
-./start $NAME
-echo "Waiting $SLEEP seconds for the VM to come up"
-sleep $SLEEP
+
+
 echo "[VM]" > ./packstack-test-hosts 
-IP=`./virt-addr.pl $NAME`
-echo $IP >> ./packstack-test-hosts
+create_vm $TEMPLATE $NAME
+
 
 
 screen -L -h 1000 -t $NAME -dmS $NAME ansible-playbook -i ./packstack-test-hosts  --extra-vars "name=$NAME branch=$BRANCH reviews=$REVIEW" $PLAYBOOK $TAG_SWITCH
