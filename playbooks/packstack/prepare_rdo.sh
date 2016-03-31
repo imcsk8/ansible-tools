a812ceb36e3fd4f1382ea2a923350c1a5db4d48c
Ivan Chavero <ichavero@redhat.com>
2016-03-31 15:22:55 -0700

    Add new delorea repos

--- playbooks/packstack/prepare_rdo.sh
+++ playbooks/packstack/prepare_rdo.sh
@@ -6,6 +6,10 @@ echo "Prepare RDO environment for installng packstack"
 BRANCH=$1
 
 case ${BRANCH} in
+    *mitaka)
+        echo "Using mitaka repos"
+        curl http://trunk.rdoproject.org/centos7-mitaka/current-passed-ci/delorean.repo | tee /etc/yum.repos.d/delorean.repo
+    ;;
     *liberty)
         echo "Using liberty repos"
         yum install -y https://repos.fedorapeople.org/repos/openstack/openstack-liberty/rdo-release-liberty-2.noarch.rpm
@@ -20,9 +24,9 @@ case ${BRANCH} in
     ;;
     *)
         echo "No branch added using delorean"
-        curl http://trunk.rdoproject.org/centos7/delorean-deps.repo |tee /etc/yum.repos.d/delorean-deps.repo
-        curl http://trunk.rdoproject.org/centos7/current/delorean.repo |tee /etc/yum.repos.d/delorean.repo
-        yum install -y https://rdoproject.org/repos/rdo-release.rpm
+        curl http://trunk.rdoproject.org/centos7-master/delorean-deps.repo |tee /etc/yum.repos.d/delorean-deps.repo
+        curl http://trunk.rdoproject.org/centos7-master/current/delorean.repo |tee /etc/yum.repos.d/delorean.repo
+        #yum install -y https://rdoproject.org/repos/rdo-release.rpm
     ;;
 
 esac
